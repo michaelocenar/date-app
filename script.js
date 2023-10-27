9447d64cd4d8f8eb8b4495c452a3a15f2e76ff4e
michaelocenar <affectuminflamma@gmail.com>
2023-10-27 00:16:37 -0600

    234

--- script.js
+++ script.js
@@ -4,35 +4,22 @@ function findDate() {
   const monthInput = document.getElementById("month").value;
 
   const resultDate = findNextDate(new Date(), dayNumberInput, weekdayInput, monthInput);
-  if (resultDate) {
-      const formattedResult = formatDate(resultDate);
-      document.getElementById("result").innerText = "Next Date: " + formattedResult;
-  } else {
-      document.getElementById("result").innerText = "No valid date found.";
-  }
+  const formattedResult = formatDate(resultDate);
+  document.getElementById("result").innerText = "Next Date: " + formattedResult;
 }
-
 
 function findNextDate(currentDate, targetDayNumber, targetWeekday, targetMonth) {
   targetDayNumber = targetDayNumber ? parseInt(targetDayNumber) : undefined;
   targetWeekday = targetWeekday ? parseInt(targetWeekday) : undefined;
   targetMonth = targetMonth !== undefined ? parseInt(targetMonth) : undefined;
 
-  let counter = 0;
-  const maxIterations = 366;  // Maximum number of days to check (covers an entire year)
   do {
       currentDate.setDate(currentDate.getDate() + 1);
-      counter++;
   } while (
-      (counter <= maxIterations) && 
-      ((targetDayNumber && currentDate.getDate() !== targetDayNumber) ||
+      (targetDayNumber && currentDate.getDate() !== targetDayNumber) ||
       (targetWeekday !== undefined && currentDate.getDay() !== targetWeekday) ||
-      (targetMonth !== undefined && currentDate.getMonth() !== targetMonth))
+      (targetMonth !== undefined && currentDate.getMonth() !== targetMonth)
   );
-
-  if (counter > maxIterations) {
-      return null; // No valid date found
-  }
 
   return currentDate;
 }
