ac8be84f01e25da61cdebd447cf119ee09d4f5a8
michaelocenar <affectuminflamma@gmail.com>
2023-10-27 00:26:14 -0600

    change back

--- script.js
+++ script.js
@@ -3,35 +3,22 @@ function findDate() {
   const weekdayInput = document.getElementById("weekday").value;
   const monthInput = document.getElementById("month").value;
 
-  const resultDate = findNextDate(new Date(), dayNumberInput, weekdayInput, monthInput);
-  if (resultDate) {
-      const formattedResult = formatDate(resultDate);
-      document.getElementById("result").innerText = "Next Date: " + formattedResult;
-  } else {
-      document.getElementById("result").innerText = "No valid date found.";
-  }
+  const result = findNextDate(new Date(), dayNumberInput, weekdayInput, monthInput);
+  document.getElementById("result").innerText = "Next Date: " + result;
 }
 
 function findNextDate(currentDate, targetDayNumber, targetWeekday, targetMonth) {
-  targetDayNumber = (targetDayNumber !== "") ? parseInt(targetDayNumber) : null;
-  targetWeekday = targetWeekday !== "" ? parseInt(targetWeekday) : null;
-  targetMonth = targetMonth !== "" ? parseInt(targetMonth) : null;
+  targetDayNumber = targetDayNumber ? parseInt(targetDayNumber) : undefined;
+  targetWeekday = targetWeekday ? parseInt(targetWeekday) : undefined;
+  targetMonth = targetMonth !== undefined ? parseInt(targetMonth) : undefined;
 
-  let counter = 0;
-  const maxIterations = 366;  // Maximum number of days to check (covers an entire year)
   do {
       currentDate.setDate(currentDate.getDate() + 1);
-      counter++;
   } while (
-      (counter <= maxIterations) && 
-      ((targetDayNumber !== null && currentDate.getDate() !== targetDayNumber) ||
-      (targetWeekday !== null && currentDate.getDay() !== targetWeekday) ||
-      (targetMonth !== null && currentDate.getMonth() !== targetMonth))
+      (targetDayNumber && currentDate.getDate() !== targetDayNumber) ||
+      (targetWeekday !== undefined && currentDate.getDay() !== targetWeekday) ||
+      (targetMonth !== undefined && currentDate.getMonth() !== targetMonth)
   );
-
-  if (counter > maxIterations) {
-      return null; // No valid date found
-  }
 
   return currentDate;
 }
