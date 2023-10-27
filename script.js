bda9f622b76c7c01e436597971bc2fc4cabfa416
michaelocenar <affectuminflamma@gmail.com>
2023-10-27 00:38:04 -0600

    fix

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ function findNextDate(currentDate, targetDayNumber, targetWeekday, targetMonth) 
   targetWeekday = targetWeekday ? parseInt(targetWeekday) : undefined;
   targetMonth = targetMonth !== undefined ? parseInt(targetMonth) : undefined;
 
-  do {
+  while (true) {  // keep searching until we find a match
       currentDate.setDate(currentDate.getDate() + 1);
-  } while (
-      (targetDayNumber !== undefined && currentDate.getDate() !== targetDayNumber) ||
-      (targetWeekday !== undefined && currentDate.getDay() !== targetWeekday) ||
-      (targetMonth !== undefined && currentDate.getMonth() !== targetMonth)
-  );
+
+      // Check if current date matches all conditions
+      const isDayMatch = !targetDayNumber || currentDate.getDate() === targetDayNumber;
+      const isWeekdayMatch = targetWeekday === undefined || currentDate.getDay() === targetWeekday;
+      const isMonthMatch = targetMonth === undefined || currentDate.getMonth() === targetMonth;
+
+      if (isDayMatch && isWeekdayMatch && isMonthMatch) {
+          break;  // if all conditions are met, exit the loop
+      }
+  }
 
   return currentDate;
 }
+
 
 function formatDate(date) {
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
