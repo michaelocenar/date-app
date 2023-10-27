7532033e451646a60c35c9dd9a4e28ed7e3d5d2d
michaelocenar <affectuminflamma@gmail.com>
2023-10-27 00:41:12 -0600

    sdfsd

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-function findDate() {
-  const dayNumberInput = document.getElementById("dayNumber").value;
-  const weekdayInput = document.getElementById("weekday").value;
-  const monthInput = document.getElementById("month").value;
-
-  const resultDate = findNextDate(new Date(), dayNumberInput, weekdayInput, monthInput);
-  const formattedResult = formatDate(resultDate);
-  document.getElementById("result").innerText = "Next Date: " + formattedResult;
-}
-
 function findNextDate(currentDate, targetDayNumber, targetWeekday, targetMonth) {
   targetDayNumber = targetDayNumber ? parseInt(targetDayNumber) : undefined;
   targetWeekday = targetWeekday ? parseInt(targetWeekday) : undefined;
   targetMonth = targetMonth !== undefined ? parseInt(targetMonth) : undefined;
 
-  while (true) {  // keep searching until we find a match
+  const maxAttempts = 365 * 10;  // a maximum of 10 years
+  for (let i = 0; i < maxAttempts; i++) {
       currentDate.setDate(currentDate.getDate() + 1);
 
-      // Check if current date matches all conditions
       const isDayMatch = !targetDayNumber || currentDate.getDate() === targetDayNumber;
       const isWeekdayMatch = targetWeekday === undefined || currentDate.getDay() === targetWeekday;
       const isMonthMatch = targetMonth === undefined || currentDate.getMonth() === targetMonth;
 
       if (isDayMatch && isWeekdayMatch && isMonthMatch) {
-          break;  // if all conditions are met, exit the loop
+          return currentDate;  // if all conditions are met, return the date
       }
   }
 
-  return currentDate;
+  // If no date matches the criteria in the next 10 years, then return null
+  return null;
+}
+function findDate() {
+const dayNumberInput = document.getElementById("dayNumber").value;
+const weekdayInput = document.getElementById("weekday").value;
+const monthInput = document.getElementById("month").value;
+
+const resultDate = findNextDate(new Date(), dayNumberInput, weekdayInput, monthInput);
+
+if (resultDate) {
+    const formattedResult = formatDate(resultDate);
+    document.getElementById("result").innerText = "Next Date: " + formattedResult;
+} else {
+    document.getElementById("result").innerText = "No matching date found in the next 10 years.";
+}
 }
 
 
