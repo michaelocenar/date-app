b4278ac1e8c2b5c47f21b3afd34e686d36899a90
michaelocenar <affectuminflamma@gmail.com>
2023-10-26 22:15:22 -0600

    change

--- script.js
+++ script.js
@@ -32,20 +32,29 @@ function findFurthestDate() {
   const currentDate = new Date();
   let furthestDate = new Date(currentDate);
   let maxDifference = 0;
+  let bestCombination = {};
+
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   for (let month = 0; month < 12; month++) {
       for (let day = 1; day <= 31; day++) {
           for (let weekday = 0; weekday < 7; weekday++) {
               const nextDate = findNextDate(new Date(currentDate), day, weekday, month);
-              const difference = (new Date(nextDate) - currentDate) / (1000 * 60 * 60 * 24);
+              const difference = (nextDate - currentDate) / (1000 * 60 * 60 * 24);
 
               if (difference > maxDifference) {
                   maxDifference = difference;
                   furthestDate = new Date(nextDate);
+                  bestCombination = {
+                      month: monthNames[month],
+                      day: day,
+                      weekday: dayNames[weekday]
+                  };
               }
           }
       }
   }
 
-  document.getElementById("result").innerText = "Furthest Date: " + formatDate(furthestDate);
+  document.getElementById("result").innerText = `Furthest Date: ${formatDate(furthestDate)} (Combination: ${bestCombination.month} ${bestCombination.day}, ${bestCombination.weekday})`;
 }
